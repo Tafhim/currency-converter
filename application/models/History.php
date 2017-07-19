16f927c7879946e229c1678e1017dae9ff3bce15
Tafhim <tafhim.ul.islam@gmail.com>
2017-07-19 08:36:48 +0600

    Update the history model

--- application/models/History.php
+++ application/models/History.php
@@ -2,6 +2,7 @@
 
 class Application_Model_History
 {
+    // Columns
     protected $_id;
     protected $_from;
     protected $_to;
@@ -15,6 +16,7 @@ class Application_Model_History
         }
     }
 
+    // Magic method for setting any attribute
     public function __set($name, $value)
     {
         $method = 'set' . $name;
@@ -24,6 +26,7 @@ class Application_Model_History
         $this->$method($value);
     }
 
+    // Magic method for getting any attribute
     public function __get($name)
     {
         $method = 'get' . $name;
@@ -33,6 +36,7 @@ class Application_Model_History
         return $this->$method();
     }
 
+    // Method for setting attributes in bulk using an array
     public function setOptions(array $options)
     {
         $methods = get_class_methods($this);
@@ -45,64 +49,76 @@ class Application_Model_History
         return $this;
     }
 
+    // Set the 'from' code
     public function setFrom($text)
     {
         $this->_from = (string)$text;
         return $this;
     }
 
+    // Get the 'from' code
     public function getFrom()
     {
         return $this->_from;
     }
 
+    // Set the 'to' code
     public function setTo($text)
     {
         $this->_to = (string)$text;
         return $this;
     }
 
+    // Get the 'to' code
     public function getTo()
     {
         return $this->_to;
     }
 
+    // Set the amount to convert from
     public function setFromAmount($float_value)
     {
         $this->_from_amount = (float)$float_value;
         return $this;
     }
 
+    // Get the amount to covnert from
     public function getFromAmount() {
         return $this->_from_amount;
     }
 
+    // Set the converted amount
     public function setToAmount($float_value)
     {
         $this->_to_amount = (float)$float_value;
         return $this;
     }
 
+    // Get the converted amount
     public function getToAmount() {
         return $this->_to_amount;
     }
 
+    // Set the timestamp
     public function setCreated($ts)
     {
         $this->_created = $ts;
         return $this;
     }
 
+    // Get the timestamp
     public function getCreated()
     {
         return $this->_created;
     }
 
+    // Set the id
     public function setId($id) {
         $this->_id = (int)$id;
         return $this;
     }
 
+    // Get the id
     public function getId()
     {
         return $this->_id;
--- application/models/HistoryMapper.php
+++ application/models/HistoryMapper.php
@@ -2,7 +2,92 @@
 
 class Application_Model_HistoryMapper
 {
+    protected $_dbTable;
 
+    // Decorator for setting the table class
+    public function setDbTable($dbTable)
+    {
+        if (is_string($dbTable)) {
+            $dbTable = new $dbTable();
+        }
+        if (!$dbTable instanceof Zend_Db_Table_Abstract) {
+            throw new Exception('Invalid table data gateway provided');
+        }
+        $this->_dbTable = $dbTable;
+        return $this;
+    }
+
+    // Decorator for table class
+    public function getDbTable()
+    {
+        if (null === $this->_dbTable) {
+            $this->setDbTable('Application_Model_DbTable_History');
+        }
+        return $this->_dbTable;
+    }
+
+    // Add a history element to the DB
+    public function save(Application_Model_History $history)
+    {
+        // Set the new values
+        $data = array(
+            'from'   => $history->getFrom(),
+            'to' => $history->getTo(),
+            'from_amount' => $history->getFromAmount(),
+            'to_amount' => $history->getToAmount(),
+            'created' => date('Y-m-d H:i:s'),
+        );
+ 
+        // If not already existing, insert
+        if (null === ($id = $history->getId())) {
+            unset($data['id']);
+            $this->getDbTable()->insert($data);
+        } else {
+            $this->getDbTable()->update($data, array('id = ?' => $id));
+        }
+    }
+
+    // Return a history element based on ID
+    public function find($id, Application_Model_History $history)
+    {
+        // Find using id
+        $result = $this->getDbTable()->find($id);
+        if (0 == count($result)) {
+            return;
+        }
+        $row = $result->current();
+
+        // Map the result
+        $history->setId($row->id)
+             ->setFrom($row->from)
+             ->setTo($row->to)
+             ->setFromAmount($row->from_amount)
+             ->setToAmount($row->to_amount)
+             ->setCreated($row->created);
+
+        return $history;
+    }
+
+    // Return all the history elements in the DB
+    public function fetchAll()
+    {
+        // Find all
+        $resultSet = $this->getDbTable()->fetchAll();
+        $entries   = array();
+
+        // Map the result in array
+        foreach ($resultSet as $row) {
+            $entry = new Application_Model_History();
+            $entry->setId($row->id)
+                ->setFrom($row->from)
+                ->setTo($row->to)
+                ->setFromAmount($row->from_amount)
+                ->setToAmount($row->to_amount)
+                ->setCreated($row->created);
+            $entries[] = $entry;
+        }
+        return $entries;
+    }
 
 }
 
