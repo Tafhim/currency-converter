504e10be7e138b0408a915272ee609a7196c77c7
Tafhim <tafhim.ul.islam@gmail.com>
2017-08-02 04:12:19 +0600

    Implement a cli router manually

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -8,5 +8,42 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view->doctype('XHTML1_STRICT');
     }
 
+    protected function _initRouter()
+    {
+        if( PHP_SAPI == 'cli' )
+        {
+            $this->bootstrap( 'FrontController' );
+            $front = $this->getResource( 'FrontController' );
+            $front->setParam('disableOutputBuffering', true);
+            require_once 'router/cli.php';
+            $front->setRouter( new Application_Router_Cli() );
+            $front->setRequest( new Zend_Controller_Request_Simple() );
+        }
+    }
+
+    protected function _initRoutes ()
+    {
+        $router = Zend_Controller_Front::getInstance ()->getRouter ();
+        if ($router instanceof Zend_Controller_Router_Rewrite)
+        {
+            // put your web-interface routes here, so they do not interfere
+        }
+    }
+
+    protected function _initError ()
+    {
+        $this->bootstrap( 'FrontController' );
+        $front = $this->getResource( 'FrontController' );
+        $front->registerPlugin( new Zend_Controller_Plugin_ErrorHandler() );
+        $error = $front->getPlugin ('Zend_Controller_Plugin_ErrorHandler');
+        $error->setErrorHandlerController('index');
+
+        if (PHP_SAPI == 'cli')
+        {
+            $error->setErrorHandlerController ('error');
+            $error->setErrorHandlerAction ('cli');
+        }
+    }
+
 }
 
