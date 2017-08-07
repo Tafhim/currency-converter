9bdf117f9a3f8ca789439b56780dfcc4b7d08aed
Tafhim <tafhim.ul.islam@gmail.com>
2017-08-07 18:27:18 +0600

    Add Swap button to form

--- application/forms/RateConverter.php
+++ application/forms/RateConverter.php
@@ -18,6 +18,8 @@ class Application_Form_RateConverter extends Zend_Form
         $convertToValue = new Zend_Form_Element_Text('convert_to');
         $convertToValue->setAttrib('disabled', "disabled");
         $submitButton = new Zend_Form_Element_Submit('Submit');
+        $swapButton = new Zend_Form_Element_Button('Swap');
+        $swapButton->setAttrib('id', 'converter-swap');
 
         // Add all the elements to the form
         $this->addElement($fromCurrency);
@@ -25,6 +27,7 @@ class Application_Form_RateConverter extends Zend_Form
         $this->addElement($convertToValue);
         $this->addElement($toCurrency);
         $this->addElement($submitButton);
+        $this->addElement($swapButton);
     }
 
 
